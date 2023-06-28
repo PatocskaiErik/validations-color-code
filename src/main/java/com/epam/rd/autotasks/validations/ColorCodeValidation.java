d75db618bd749a6f331224203fda743ea95f151a
Erik <patoerik93@gmail.com>
2023-06-28 17:06:29 +0200

    Added implementation

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -5,13 +5,15 @@ import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
+        if(color == null) {
+            return false;
+        }
 
-        // Put your code here
+        String regex = "^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(color);
+
+        return matcher.matches();
 
     }
 }
-
-
-
-
-
